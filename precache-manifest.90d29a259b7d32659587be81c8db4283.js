836ab8f5b7678baccc7242165d92a7f6dc21a859
Satyajeet Desale <Satyajeet@Satyajeets-Mac-mini.local>
2020-07-24 23:48:55 +0530

    Updates

---
+++ precache-manifest.90d29a259b7d32659587be81c8db4283.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f00e170ae5f8f63bcabdf359f81cecb",
+    "url": "/my-profile/index.html"
+  },
+  {
+    "revision": "7cac5f9d10e9d7b65e2c",
+    "url": "/my-profile/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "2d89dabfaec1f03cf799",
+    "url": "/my-profile/static/css/main.5e2a8683.chunk.css"
+  },
+  {
+    "revision": "7cac5f9d10e9d7b65e2c",
+    "url": "/my-profile/static/js/2.85cfc7b7.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/my-profile/static/js/2.85cfc7b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d89dabfaec1f03cf799",
+    "url": "/my-profile/static/js/main.cb8572c4.chunk.js"
+  },
+  {
+    "revision": "c1d853bd7e89e86f5c06",
+    "url": "/my-profile/static/js/runtime-main.58046206.js"
+  },
+  {
+    "revision": "cc078057391fd48c1777b65a398ff337",
+    "url": "/my-profile/static/media/profile_pic.cc078057.png"
+  }
+]);
